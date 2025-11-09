4662e20d6b9fa43795114197c791f9708c88c951
KLASTER-DIGITAL <159112207+KLASTER-KZ@users.noreply.github.com>
2025-11-09 11:57:37 +0400

    🔧 chore: Final service worker and script updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'achievement-diary-v091d62b';
-const CACHE_NAME_API = 'achievement-diary-api-v091d62b';
-const CACHE_NAME_STATIC = 'achievement-diary-static-v091d62b';
+const CACHE_NAME = 'achievement-diary-vd8d1a19';
+const CACHE_NAME_API = 'achievement-diary-api-vd8d1a19';
+const CACHE_NAME_STATIC = 'achievement-diary-static-vd8d1a19';
 
 // Cache TTL (Time To Live) в миллисекундах
 const CACHE_TTL = {
