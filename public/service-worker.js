ba00eddc0b5757b2bf9a1e3e963e82c14fd87074
KLASTER-DIGITAL <159112207+KLASTER-KZ@users.noreply.github.com>
2025-11-26 00:09:51 +0400

    chore(pwa): Force cache bust - update SW to v58d29eb
    Running prebuild manually to update service worker cache names
    with latest commit hash. This will force all users to see the
    update prompt and get the latest fixes for books system.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'achievement-diary-v823eea2';
-const CACHE_NAME_API = 'achievement-diary-api-v823eea2';
-const CACHE_NAME_STATIC = 'achievement-diary-static-v823eea2';
+const CACHE_NAME = 'achievement-diary-v58d29eb';
+const CACHE_NAME_API = 'achievement-diary-api-v58d29eb';
+const CACHE_NAME_STATIC = 'achievement-diary-static-v58d29eb';
 
 // Cache TTL (Time To Live) в миллисекундах
 const CACHE_TTL = {
